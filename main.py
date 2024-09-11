433f359f7b2bbef03a04e3dc8870c92a822f0ba9
Ollisha Carpenter <ocarpenter@luc.edu>
2024-09-11 03:11:46 +0000

    completed lab and homework

--- main.py
+++ main.py
@@ -91,9 +91,23 @@ def merge_lists(list1: list, list2: list) -> list:
     Returns:
     - list: A new sorted list containing all elements from list1 and list2
     """
-    
-    # TODO: Implement this function
-    pass
+    merged_list = []
+    i, j = 0, 0
+
+    while i < len(list1) and j < len(list2) :
+        if list1[i] < list2[j]:
+            merged_list.append(list1[i])
+            i += 1
+        else:
+            merged_list.append(list2[j])
+            j += 1
+    while i < len(list1):
+        merged_list.append(list1[i])
+        i += 1
+    while j < len(list2):
+        merged_list.append(list2[j])
+        j += 1
+    return merged_list
 
 
 # Unit Tests for merge_lists
